f76dc9408ddd4be1206d8e258da2c6ed88ea9df6
Fosa1990 <fosa1990@gmail.com>
2021-12-01 14:12:19 +0200

    added forms (have a bug)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
+// import SignUpForm from './components/SignUpForm';
+// import LoginForm from './components/LoginForm';
+// import Form from './components/Form/';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Form></Form>
+        {/* <SignUpForm></SignUpForm> */}
+        {/* <LoginForm></LoginForm> */}
+        {/* <Form></Form> */}
       </header>
     </div>
   );
