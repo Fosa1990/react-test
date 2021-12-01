34a1c8638f1f2371481469c76160f2d796ed2518
Fosa1990 <fosa1990@gmail.com>
2021-12-01 11:58:46 +0200

    added 2 methods of Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,19 +12,24 @@ export class Form extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.todoSubmit = this.todoSubmit.bind(this);
+    this.clearFields = this.clearFields.bind(this);
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
   }
+
   handleChange(event) {
     this.setState({
       input: event.target.value,
     });
   }
 
+  // var 1 [ 1 method = 1 action ] start
   handleSubmit(event) {
     event.preventDefault();
     this.setState({
       submit: this.state.input,
     });
   }
+
   todoSubmit(event) {
     event.preventDefault();
     this.setState({
@@ -32,11 +37,31 @@ export class Form extends Component {
       items: [...this.state.items, this.state.input],
     });
   }
+  // var 1 [ 1 method = 1 action ] end
+
+  // var 2 [ 1 method = 2 actions ] start
+  handleTodoSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      input: this.state.input,
+      submit: this.state.input,
+      items: [...this.state.items, this.state.input],
+    });
+  }
+  // var 2 [ 1 method = 2 actions ] end
+
+  clearFields() {
+    this.setState({
+      input: '',
+      submit: '',
+      items: [],
+    });
+  }
 
   render() {
     return (
       <div>
-        <form onSubmit={(this.handleSubmit, this.todoSubmit)}>
+        <form onSubmit={this.handleTodoSubmit}>
           <input
             value={this.state.input}
             onChange={this.handleChange}
@@ -44,6 +69,9 @@ export class Form extends Component {
           />
           <button type="submit">Submit</button>
         </form>
+        <button onClick={this.clearFields} type="button">
+          Clear All
+        </button>
         <h3>Input: {this.state.input}</h3>
         <h3>Submit: {this.state.submit}</h3>
         <ul>
