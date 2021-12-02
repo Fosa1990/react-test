4b44700e4b0af02ad570f6c123e163189709f3f5
Fosa1990 <fosa1990@gmail.com>
2021-12-02 21:19:18 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form/';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import SignUpFormCheckbox from './components/SignUpFormCheckbox';
+import RadioButtons from './components/RadioButtons';
 import SignUpFormFinal from './components/SignUpFormFinal';
-// import RadioButtons from './components/RadioButtons';
-// import SignUpFormCheckbox from './components/SignUpFormCheckbox';
-// import SignUpForm from './components/SignUpForm';
-// import LoginForm from './components/LoginForm';
-// import Form from './components/Form/';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Form></Form>
+        <hr />
+        <LoginForm></LoginForm>
+        <hr />
+        <SignUpForm></SignUpForm>
+        <hr />
+        <SignUpFormCheckbox></SignUpFormCheckbox>
+        <hr />
+        <RadioButtons></RadioButtons>
+        <hr />
         <SignUpFormFinal></SignUpFormFinal>
-        {/* <RadioButtons></RadioButtons> */}
-        {/* <SignUpFormCheckbox></SignUpFormCheckbox> */}
-        {/* <SignUpForm></SignUpForm> */}
-        {/* <LoginForm></LoginForm> */}
-        {/* <Form></Form> */}
       </header>
     </div>
   );
