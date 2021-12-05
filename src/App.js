301c74fa2aa3adbf543c84ddb79152fa1ebc3581
Fosa1990 <fosa1990@gmail.com>
2021-12-05 21:58:18 +0200

    added new react routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form/';
 import LoginForm from './components/LoginForm';
@@ -9,12 +8,68 @@ import SignUpFormFinal from './components/SignUpFormFinal';
 import Lifecycle from './components/Lifecycle';
 import Title from './components/Title';
 import FetchApi from './components/FetchApi';
+import { Link, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import styled from 'styled-components';
+
+const LinkRouteStyled = styled(Link)`
+  color: var(--yellow);
+  text-decoration: none;
+`;
+
+const Component1 = () => {
+  return (
+    <Fragment>
+      <hr />
+      <p>
+        <span style={{ color: 'var(--green)' }}>Routing to: </span>
+        <span style={{ color: 'var(--red)' }}>Component "1" </span>
+        <span style={{ color: 'var(--white)' }}>watch to URL (^_^)</span>
+      </p>
+      <hr />
+    </Fragment>
+  );
+};
+const Component2 = () => {
+  return (
+    <Fragment>
+      <hr />
+      <p>
+        <span style={{ color: 'var(--green)' }}>Routing to: </span>
+        <span style={{ color: 'var(--red)' }}>Component "2" </span>
+        <span style={{ color: 'var(--white)' }}>watch to URL (^_^)</span>
+      </p>
+      <hr />
+    </Fragment>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <hr />
+        <LinkRouteStyled to="/component1">
+          <span style={{ color: 'var(--green)' }}>New React </span>
+          Routing: Link to Components 1
+        </LinkRouteStyled>
+        <LinkRouteStyled to="/component2">
+          <span style={{ color: 'var(--green)' }}>New React </span>
+          Routing: Link to Components 2
+        </LinkRouteStyled>
+        <hr />
+        <Routes>
+          <Route path="/component1" element={<Component1 />} />
+          <Route path="/component2" element={<Component2 />} />
+        </Routes>
+        <hr />
+        <Title title={'Component: FetchApi'}></Title>
+        <FetchApi></FetchApi>
+        <hr />
+        <Title title={'Component: Lifecycle'}></Title>
+        <p>Open console (^_^)</p>
+        <Lifecycle></Lifecycle>
+        <hr />
         <Title title={'Component: Form'}></Title>
         <Form></Form>
         <hr />
@@ -33,12 +88,6 @@ function App() {
         <Title title={'Component: SignUpFormFinal'}></Title>
         <SignUpFormFinal></SignUpFormFinal>
         <hr />
-        <Title title={'Component: Lifecycle'}></Title>
-        <p>Open console (^_^)</p>
-        <Lifecycle></Lifecycle>
-        <hr />
-        <Title title={'Component: FetchApi'}></Title>
-        <FetchApi></FetchApi>
       </header>
     </div>
   );
